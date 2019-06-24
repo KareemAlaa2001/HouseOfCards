a1cd60c344143b24e85db7d5c25c42940d206e52
Mostafa <tifa70x@gmail.com>
2019-06-24 07:46:05 +0200

    threecard class

--- src/main/java/ThreeCard.java
+++ src/main/java/ThreeCard.java
@@ -1,4 +1,15 @@
 
-public class ThreeCard {
+public class ThreeCard extends Card{
+
+	public ThreeCard(double attackPoints, House house) {
+		super(attackPoints, house);
+		
+}
+// the method below can direct attack any player over troops, and the player who triggers that effect can also attack him/herself.
+	public void directAttack(InGamePlayer player) {
+		Attack.attackPlayer(player);
+	}
+	
+	
 
 }
